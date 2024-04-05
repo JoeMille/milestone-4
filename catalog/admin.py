b3dd093e762f3fff7ea054eb6e403714e1c75d37
Joe Miller <98966875+JoeMille@users.noreply.github.com>
2024-04-05 15:40:31 +0100

    Successful creation of further product detail and image fields for immersive user experience when directed to product_detail.html

--- catalog/admin.py
+++ catalog/admin.py
@@ -4,8 +4,11 @@ from .models import Category, Product, Basket, BasketItem, Review, ContactMessag
 class ContactMessageAdmin(admin.ModelAdmin):
     list_display = ('title', 'email', 'message', 'created_at')  # fields to display in the list view
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'image', 'description', 'price', 'description2', 'image2', 'image3', 'image4']
+
 admin.site.register(ContactMessage, ContactMessageAdmin)  # register ContactMessageAdmin with ContactMessage
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)  # register ProductAdmin with Product
 admin.site.site_header = 'Cosmic Commerce Admin Portal'
 admin.site.site_title = 'Admin Operations'
